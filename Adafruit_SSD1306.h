c1f3923339a99fe5bb15a9c30c4c29ddafd9d27b
Richard Ulrich <richi@paraeasy.ch>
2015-09-04 23:42:38 +0200

    started optimizing for less RAM usage

--- Adafruit_SSD1306.h
+++ Adafruit_SSD1306.h
@@ -167,4 +167,6 @@ class Adafruit_SSD1306 : public Adafruit_GFX {
   inline void drawFastVLineInternal(int16_t x, int16_t y, int16_t h, uint16_t color) __attribute__((always_inline));
   inline void drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color) __attribute__((always_inline));
 
+ private:
+  uint8_t buffer[SSD1306_LCDHEIGHT * SSD1306_LCDWIDTH / 8];
 };
